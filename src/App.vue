35352aabaf742e2298d2c263d74083ae95d7ba27
unknown <651507008@qq.com>
2020-08-03 17:16:57 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>hi</div>
+  <Home  msg="Hello Vue 3.0 + Vite" />
+</template>
+
+<script>
+
+import Home from './components/Home.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Home
+  }
+}
+</script>
